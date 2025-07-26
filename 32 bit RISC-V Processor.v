0723196c4f8d69fbeb745222991f32ebb9dc47b6
cul12 <cul123.ag@gmail.com>
2025-07-26 22:41:42 +0530

    Update 32 bit RISC-V Processor.v

--- 32 bit RISC-V Processor.v
+++ 32 bit RISC-V Processor.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: Anushka Tripathi
+// Engineer: Anushka Gupta
 // 
 // Create Date: 12.03.2025 19:19:55
 // Design Name: 
